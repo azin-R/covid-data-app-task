62b95baca16e41c998a24d8f5396731e0522ffae
Azin <azinazar.1618@gmail.com>
2023-02-22 11:00:30 +0330

    Initial Commit

---
+++ covid data app/covid_data_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  covid_data_appApp.swift
+//  covid data app
+//
+//  Created by azin on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct covid_data_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ covid data appUITests/covid_data_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  covid_data_appUITestsLaunchTests.swift
+//  covid data appUITests
+//
+//  Created by azin on 2/22/23.
+//
+
+import XCTest
+
+final class covid_data_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
